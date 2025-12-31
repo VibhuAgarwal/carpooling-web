334c689fb3f965ee57877f6d28818cc818173195
Vibhor Agarwal <vibhor.x.agarwal@goindigo.in>
2025-12-31 19:04:29 +0530

    Added project name and icon on metadat and navbar as well

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,29 @@ import "./globals.css";
 import Providers from "./providers";
 import Navbar from "./components/Navbar";
 import GoogleMapsProvider from "@/app/providers/GoogleMapsProvider";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  applicationName: "CoRide",
+  title: {
+    default: "CoRide",
+    template: "%s | CoRide",
+  },
+  icons: {
+    icon: "/RideSHareHub.png",
+    apple: "/public/RideShareHub.png",
+  },
+  openGraph: {
+    title: "CoRide",
+    siteName: "CoRide",
+    images: [{ url: "/public/RideShareHub.png" }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CoRide",
+    images: ["/public/RideShareHub.png"],
+  },
+};
 
 export default function RootLayout({
   children,
